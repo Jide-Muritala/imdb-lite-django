98972c65700318877855e99dbe50783c276425ec
Babajide Muritala <jide142000@yahoo.com>
2024-04-05 19:50:07 +0200

    modify base_dir

--- methinks/settings.py
+++ methinks/settings.py
@@ -17,7 +17,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
