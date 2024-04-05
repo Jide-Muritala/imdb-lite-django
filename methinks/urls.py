9b6fe99b62779e835d271b112e415bc08512521f
Babajide Muritala <jide142000@yahoo.com>
2024-04-05 16:10:45 +0200

    Update urls

--- methinks/urls.py
+++ methinks/urls.py
@@ -21,5 +21,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('image_challenge.urls')),
-    path('', TemplateView.as_view(template_name='image_challenge/image_list.html')),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
